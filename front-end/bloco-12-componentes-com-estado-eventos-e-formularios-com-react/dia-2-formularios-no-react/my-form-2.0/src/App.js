2576c3ff44cd8b012aabfcae1915f34afda76581
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-07 11:01:28 -0300

    adicionando todos os campos de input

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -8,6 +8,11 @@ class App extends React.Component {
     this.state = {
       name: '',
       email: '',
+      CPF: 0,
+      endereco: '',
+      cidade: '',
+      estado: '',
+      tipo: '',
     }
     this.handleSetState = this.handleSetState.bind(this);
   }
@@ -29,6 +34,15 @@ class App extends React.Component {
 
           <Inputs type="email" name="email" onChange={this.handleSetState} value={this.state.email}>Email: </Inputs>
 
+          <Inputs type="number" name="CPF" onChange={this.handleSetState} value={this.state.CPF}>CPF: </Inputs>
+
+          <Inputs type="text" name="endereco" onChange={this.handleSetState} value={this.state.endereco}>Endereço: </Inputs>
+
+          <Inputs type="text" name="cidade" onChange={this.handleSetState} value={this.state.cidade}>Cidade: </Inputs>
+
+          <Inputs type="text" name="estado" onChange={this.handleSetState} value={this.state.estado}>Estado: </Inputs>
+
+          <Inputs type="text" name="tipo" onChange={this.handleSetState} value={this.state.tipo}>Tipo: </Inputs>
 
         </fieldset>
       
