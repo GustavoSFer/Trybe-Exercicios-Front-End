9691af94b6a02bf437cbad1210e2c3c639adbd3c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2021-09-07 10:23:39 -0300

    adicionando dados pessoais name com letra maiuscula

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+    }
+    this.handleSetState = this.handleSetState.bind(this);
+  }
+
+  handleSetState({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value.toUpperCase(),
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <fieldset>
+          <legend>Dados pessoais:</legend>
+          <label htmlFor="name">
+            Name:
+            <input type="text" id="name" name="name" onChange={this.handleSetState} value={this.state.name} />
+          </label>
+        </fieldset>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
