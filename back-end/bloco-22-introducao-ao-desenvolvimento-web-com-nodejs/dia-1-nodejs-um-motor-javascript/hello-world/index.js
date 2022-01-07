e4f3fbbd98e342404013d77461756abdfeb3968c
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-01-07 18:46:03 -0300

    criando a leitura de nome e idade

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+const name = readline.question('Informe seu nome: ');
+const age = readline.questionInt('informe sua idade: ');
+
+console.log(`Olá ${name}, voce tem ${age} anos!!!`);
+// console.log('Hello, world!');
