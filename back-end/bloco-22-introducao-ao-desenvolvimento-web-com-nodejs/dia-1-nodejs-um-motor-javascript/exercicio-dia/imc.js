036a695136d48209cf91b1edad2b3c4fe631ca1b
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-01-07 19:26:35 -0300

    criando a tabela de informaçao

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-dia/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-dia/imc.js
@@ -6,6 +6,26 @@ const imc = () => {
 
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
   console.log(`o seu IMC é de: ${imc}`);
+
+  if (imc < 18.5) {
+    console.log('Abaixo do peso (magreza)');
+    return;
+  }
+  if (imc >= 18.5 && imc <= 24.9) {
+    console.log('Peso normal');
+  }
+  if (imc >= 25.0 && imc <= 29.9) {
+  console.log('Acima do peso (sobrepeso)');
+  }
+  if (imc >= 30.0 && imc <= 34.9) {
+    console.log('Obesidade grau I');
+  }
+  if (imc >= 35.0 && imc <= 39.9) {
+    console.log('Obesidade grau II');
+  }
+  if (imc > 40) {
+    console.log('Obesidade graus III e IV');
+  }
 }
 
 imc();
