8c2f73d122f23f51a374e671bddde970a9f0817e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-01-07 19:11:43 -0300

    solicitando informaçoes pelo usuario pelo readline-sync

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-dia/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/exercicio-dia/imc.js
@@ -1,7 +1,11 @@
+const readline = require("readline-sync");
 
-const imc = (peso, altura) => {
+const imc = () => {
+  const peso = readline.questionInt("Informe seu peso: ");
+  const altura = readline.questionFloat("informe sua altura: ");
+
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
   console.log(`o seu IMC é de: ${imc}`);
 }
 
-imc(70, 167);
+imc();
