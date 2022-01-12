f5ae46858dd4bfead1d4455f19e2b2b8c814524f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-01-12 18:44:43 -0300

    criando teste para leitura de arquivo

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicio1/exercicio2.test.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicio1/exercicio2.test.js
@@ -7,9 +7,7 @@ describe('função que escreverá um conteúdo em um arquivo específico.', () =
   describe('Se a escrita foi com sucesso', () => {
     it('escrita com sucesso retorno um "ok"', () => {
       const res = leituraEscrita('arq1.txt', 'Enviando teste')
-      const  te = fs.readFile('arq1.txt', 'utf-8', (err, resposta) => {
-        return 'ok'
-      });
+      
 
       expect(te).to.be.equal('ok');
     });
