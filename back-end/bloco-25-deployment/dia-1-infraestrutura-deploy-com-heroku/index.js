db7f5c56240f7e839d599b6741c52b781e151c73
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-02-12 20:29:02 -0300

    comandos do deploy escrito

--- back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/index.js
+++ back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/index.js
@@ -7,4 +7,14 @@ app.get('/', (req, res) => {
   res.status(200).send('Está vivo!!!');
 })
 
+/**
+heroku create
+git remote -v  ==> Tem que aparecer a url do heroku com o nome heroku
+git add .
+git commit -m 'mensagem'
+git push heroku master ==> sempre necessario enviar para a master do heroku e informando antes que é para o heroku.
+
+===> Com isso é feito o deploy
+ */
+
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
