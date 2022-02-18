e0c94752d2755ef08f3e00bc62b468eca3ad57af
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-02-18 15:10:52 -0300

    criando enum

---
+++ back-end/bloco-26-typescript/dia-2-tipagem-estatica-e-generics/fixao/enum.ts
@@ -0,0 +1,34 @@
+enum Semana  {
+  domingo = 1,
+  segunda,
+  terça,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+}
+
+enum RainbowColors {
+  Red = "Vermelho",
+  Orange = "Laranja",
+  Yellow = "Amarelo",
+  Green = "Verde",
+  Blue = "Azul",
+  Indigo = "Anil",
+  Violet = "Violeta",
+}
+
+enum acoes {
+  salvar,
+  imprimir,
+  abrir,
+  visualizar,
+  fechar
+}
+
+enum pontosCardiais {
+  norte = "N",
+  leste = "L",
+  sul = "S",
+  oente = "O",
+}
