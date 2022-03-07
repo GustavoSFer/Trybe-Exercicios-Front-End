7444d49b635a3e03a43bb14ef6fc8f3fcebfe75e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-03-07 15:35:58 -0300

    criando uma classe que extende da super e sobrescrevemos os metodos

--- back-end/bloco-27-programacao-orientada-a-objeto/dia-3-polimorfismo/SuperSmashBros.ts
+++ back-end/bloco-27-programacao-orientada-a-objeto/dia-3-polimorfismo/SuperSmashBros.ts
@@ -3,3 +3,15 @@ abstract class Character {
 
   abstract specialMove(): void;
 }
+
+class MeleeCharacter extends Character {
+  constructor(private name: string, private specialMoveName: string) {
+    super();
+  }
+  talk(): void {
+   console.log(`Olá, eu sou ${this.name}, eu ataco de perto.`);   
+  }
+  specialMove(): void {
+    console.log(`Movimento especial ${this.specialMoveName}`);
+  }
+}
