e0fc14c90d0bf6e5ac0c8dc13ffe6078d98d02b3
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-25 20:13:13 -0300

    Feat: criando função que imprime um triangulo com *, sendo a base o numero informado

---
+++ computer-science/ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/bonus2.py
@@ -0,0 +1,9 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um 
+# triângulo retângulo com n asteriscos de base. Por exemplo, para n = 5 o triângulo 
+# terá 5 asteriscos na base
+
+def triangulo(n):
+  for numero in range(1, n + 1):
+    print(numero * "*")
+  
+triangulo(5)
