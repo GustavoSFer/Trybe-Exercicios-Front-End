0e09186c1f1c8e94055587b1f32a2278fe872f91
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-25 18:54:18 -0300

    Feat: criando função que impreme um quadrado de *

---
+++ computer-science/ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercice3.py
@@ -0,0 +1,6 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+def quadrado(n):
+  for q in range(n):
+    print(n * '*')
+
+quadrado(5)
