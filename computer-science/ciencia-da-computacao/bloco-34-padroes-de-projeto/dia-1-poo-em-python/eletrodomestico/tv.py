ddb8a5330032c3e0f16f229c2fe20c80c5c61b96
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-03 19:30:06 -0300

    criando o metodo de aumentar volume e fazendo o teste

--- computer-science/ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/eletrodomestico/tv.py
+++ computer-science/ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/eletrodomestico/tv.py
@@ -1,6 +1,16 @@
 class Tv():
   def __init__(self, tamanho):
-    self.volume = 50
-    self.canal = 1
-    self.tamanho = tamanho
-    self.ligada = False
+    self._volume = 50
+    self._canal = 1
+    self._tamanho = tamanho
+    self._ligada = False
+
+  def aumentar_volume(self):
+    try:
+      if self._volume <= 99 and self._volume >= 0:
+        self._volume += 1
+    except ValueError():
+      print('Não pode ser alterado o volume')
+
+  def get_volume(self):
+    return self._volume
--- computer-science/ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/test/test_Controle.py
+++ computer-science/ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/test/test_Controle.py
@@ -3,6 +3,13 @@ from eletrodomestico.tv import Tv
 def test_create_Tv():
   tv = Tv(32)
   
-  assert tv.volume == 50
-  assert tv.canal == 1
-  assert tv.ligada == False
+  assert tv._volume == 50
+  assert tv._canal == 1
+  assert tv._ligada == False
+
+def test_aumentar_volume():
+  tv = Tv(50)
+  tv.aumentar_volume()
+  volume = tv.get_volume()
+
+  assert volume == 51
